7be642edf2dea88db291aab810d5779822fd3086
Okan Aslan <okannaslann@hotmail.com>
2023-06-17 18:56:39 +0300

    project started

---
+++ PodCastApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodCastApp
+//
+//  Created by MacBook on 17.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
